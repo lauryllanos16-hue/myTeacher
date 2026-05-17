8d33beb3e837d8c4a82e7bffda0ae44f18f61e45
COROSKY <andrescoro2129@gmail.com>
2026-05-17 11:48:04 -0500

    Terminando perfil inicio estudiante

--- backend/db.js
+++ backend/db.js
@@ -7,6 +7,8 @@ const pool = mysql.createPool({
   user:     process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  charset: 'utf8mb4',
+  timezone: 'Z',
   waitForConnections: true,
   connectionLimit:    10,
   queueLimit:         0,
